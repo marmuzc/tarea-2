0538e9badd9856ca9492e1494f77b2a030fe0565
Mariel <mariemunoz2022@udec.cl>
2024-10-08 11:32:47 -0300

    Agregada getter y setter de departamento

--- Departamento.java
+++ Departamento.java
@@ -1,11 +1,20 @@
 public class Departamento {
     private String nombre;
 
-    public Departamento(){
+    public Departamento(String nombre){
+        this.nombre = nombre;
     }
 
     private int obtenerCantidadEmpleados(){
         return 0;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = getNombre();
     }
 
 }
