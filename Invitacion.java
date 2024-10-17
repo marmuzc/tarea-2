b0a4a9b67eda3fb35bd8996c6c230e0fc152e6be
Mariel M <mariemunoz2022@udec.cl>
2024-10-17 14:26:48 -0300

    Ahora invitacion contendria una lista de los Invitados, se agrega metodo agregarInvitado

--- Invitacion.java
+++ Invitacion.java
@@ -1,24 +1,34 @@
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Invitacion {
-    private Instant hora; // Hora de la invitación
+    private Instant hora;
+    private List<Invitable> invitados;
 
     public Invitacion(Instant hora) {
-        this.hora = hora; // Hora de invitación
+        this.hora = hora;
+        this.invitados = new ArrayList<>();
     }
 
     public Instant getHora() {
         return hora;
     }
 
-    public void setHora(Instant hora){ //No definitivo
+    public void setHora(Instant hora) {
         this.hora = hora;
+    }
+
+    public void agregarInvitado(Invitable invitado) {
+        invitados.add(invitado);
+        invitado.invitar(this);
     }
 
     @Override
     public String toString() {
         return "Invitacion{" +
-                ", hora=" + hora +
+                "hora=" + hora +
+                ", invitados=" + invitados +
                 '}';
     }
 }
