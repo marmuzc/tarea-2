05e9289a964895b775ba3b6514f6212af18d5167
Mariel M <mariemunoz2022@udec.cl>
2024-10-19 00:42:12 -0300

    Arreglado las reuniones y su herencia

--- ReunionPresencial.java
+++ ReunionPresencial.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
 
-public class ReunionPresencial {
+class reunionPresencial extends Reunion {
+    private String sala;
+
+    public reunionPresencial(Date fecha, Instant horaPrevista, Duration duracionPrevista, String sala) {
+        super(fecha, horaPrevista, duracionPrevista);
+        this.sala = sala;
+    }
+
+    public String getSala() {
+        return sala;
+    }
+
+    public void setSala(String sala) {
+        this.sala = sala;
+    }
+
+    @Override
+    public void iniciar() {
+        super.iniciar(); // Llamamos al método de la clase padre para registrar la hora de inicio
+        System.out.println("Reunión presencial iniciada en la sala: " + sala + " a las: " + getHorarioInicio());
+    }
+
+    @Override
+    public void finalizar() {
+        super.finalizar(); // Llamamos al método de la clase padre para registrar la hora de fin
+        System.out.println("Reunión presencial finalizada en la sala: " + sala + " a las: " + getHoraFin());
+    }
+
+    @Override
+    public String toString() {
+        // Llamamos al toString() de la clase padre y agregamos la información adicional de la sala
+        return super.toString() + ", sala='" + sala + '\'';
+    }
 }
